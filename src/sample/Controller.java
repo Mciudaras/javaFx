1ea3813578f767063c28968d835b83909a3e1a26
Mindaugas <m.ciudaras@gmail.com>
2017-04-13 12:08:21 +0300

    commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,14 +3,16 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
-
     @FXML
     private Label text;
-
+    @FXML
+    private TextField textField1;
+    @FXML
+    private  TextField textField2;
     public void sayHi(ActionEvent event){
-        String prefix = text.getText();
-        text.setText(prefix + " Mindaugas");
+        text.setText(textField1.getText() +" "+ textField2.getText());
     }
 }
